51ece7e4178e6155c74df4df998c61930e8dc5bd
KCorderoC <20241575@itla.edu.do>
2026-04-24 01:08:53 -0400

    proyecto limpio devops

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hola Mundo DevOps 🚀');
+});
+
+module.exports = app;
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
+});
---
+++ test/app.test.js
@@ -0,0 +1,9 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('GET /', () => {
+    it('debe devolver Hola Mundo', async () => {
+        const res = await request(app).get('/');
+        expect(res.text).toBe('Hola Mundo DevOps 🚀');
+    });
+});
